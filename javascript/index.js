37d1a5068c47afbc0e42900f99b28d3729689610
Janice Chan <janice.chan1288@gmail.com>
2022-04-12 21:33:30 -0400

    updated renderProducts function

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,10 @@
 const BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=benefit'
-const allProductsDiv = document.getElementById('product-container');
-const productCardDiv = document.getElementById('product-card-container');
-
-
+const productCardContainer = document.getElementById('product-card-container');
+//const imageContainer = document.getElementById('image-container');
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchAPI();
-    fetchImages();
+    
 })
 
 //Get data from API and for each product send it to the renderProducts function
@@ -18,31 +16,22 @@ const fetchAPI = () => {
     })
 }
 
-const fetchImages = () => {
-    fetch(BASE_URL)
-    .then(resp => resp.json())
-    .then(products => {
-        products.forEach(product => product["api_featured_image"])
-    })
-}
-
 //Helper functions
 const createElem = tag => document.createElement(tag);
 
 const renderProducts = (product) => {
-    fetchImages();
     const productCard = createElem('div')
     productCard.className = 'product-card'
 
     const image = createElem('img');
     image.src = product["api_featured_image"];
-
+      
     const name = createElem('p');
     name.textContent = product.name;
 
     const price = createElem('p');
     price.textContent = product.price;
 
-    productCardDiv.append(productCard);
+    productCardContainer.appendChild(productCard);
     productCard.append(image, name, price);
 } 
